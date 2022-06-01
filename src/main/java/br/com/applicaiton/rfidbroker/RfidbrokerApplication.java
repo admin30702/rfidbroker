dd5bb14c5d167b9caf80515a4545971c7c852788
Marcio Oliveira <marcio.oliveira@ifms.edu.br>
2022-06-01 19:14:01 -0300

    Commit initial

---
+++ src/main/java/br/com/applicaiton/rfidbroker/RfidbrokerApplication.java
@@ -0,0 +1,13 @@
+package br.com.applicaiton.rfidbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfidbrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RfidbrokerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/applicaiton/rfidbroker/RfidbrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.applicaiton.rfidbroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfidbrokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
